a4affd70a403982acc5a2ae47b3b05420ed9a236
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-27 17:51:11 -0500

    [add] CRUD SubGenero DAO

---
+++ JavaExperto/project-JPA/src/main/java/com/nestor/projectjpa/DAO/SubGeneroDAO.java
@@ -0,0 +1,45 @@
+package com.nestor.projectjpa.DAO;
+
+import com.nestor.projectjpa.entity.SubGenero;
+
+import java.util.List;
+
+/**
+ * The interface Sub genero dao.
+ *
+ * @author Nestor Monroy Interface DAO que realiza el CRUD con JPA para la tabla de subgenero.
+ */
+public interface SubGeneroDAO {
+    /**
+     * Método que permite guardar un subgénero.
+     * @param subGenero  {@link SubGenero} objeto a guardar.
+     */
+    void guardar(SubGenero subGenero);
+
+    /**
+     * Método que permite actualizar un subgénero.
+     * @param subGenero {@link SubGenero} objeto a actualizar.
+     */
+    void actualizar(SubGenero subGenero);
+
+    /**
+     * Método que permite eliminar un subgénero por su identificador.
+     * @param id {@link Long} identificador del subgénero a eliminar.
+     */
+    void eliminiar(Long id);
+
+    /**
+     * Método que permite consultar la lista de subgéneros.
+     * @return {@link List} lista de subgéneros consultados.
+     */
+    List<SubGenero> consulta();
+
+    /**
+     * Método que permite consultar un SubGenero a partir de su identificador.
+     * @param id {@link Long} identificador del subgénero a consultar.
+     * @return {@link SubGenero} un objeto subgénero consultado.
+     */
+    SubGenero consultarById(Long id);
+
+
+}
---
+++ JavaExperto/project-JPA/src/main/java/com/nestor/projectjpa/DAO/SubGeneroDAOImpl.java
@@ -0,0 +1,32 @@
+package com.nestor.projectjpa.DAO;
+
+import com.nestor.projectjpa.entity.SubGenero;
+
+import java.util.List;
+
+public class SubGeneroDAOImpl implements SubGeneroDAO {
+    @Override
+    public void guardar(SubGenero subGenero) {
+
+    }
+
+    @Override
+    public void actualizar(SubGenero subGenero) {
+
+    }
+
+    @Override
+    public void eliminiar(Long id) {
+
+    }
+
+    @Override
+    public List<SubGenero> consulta() {
+        return null;
+    }
+
+    @Override
+    public SubGenero consultarById(Long id) {
+        return null;
+    }
+}
