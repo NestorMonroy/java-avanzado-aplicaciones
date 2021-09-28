0b7f7c372a22aa010889d91b5cd812d375f74a64
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-28 13:31:59 -0500

    [add] example context annotation vs bean xhtml

---
+++ JavaExperto/project-SPRING-web/src/main/java/com/nestor/projectspringweb/services/SpringService.java
@@ -0,0 +1,13 @@
+package com.nestor.projectspringweb.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author NestorMonroy
+ * Interface de prueba para implementar metodos y se pueda utilizar anotaciones con SPRING.
+ */
+
+@Service
+public interface SpringService {
+
+}
---
+++ JavaExperto/project-SPRING-web/src/main/java/com/nestor/projectspringweb/services/imlp/SpringServiceDAOImpl.java
@@ -0,0 +1,10 @@
+package com.nestor.projectspringweb.services.imlp;
+
+import com.nestor.projectspringweb.services.SpringService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SpringServiceDAOImpl implements SpringService {
+
+}
