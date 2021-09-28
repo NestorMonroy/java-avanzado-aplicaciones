6d03c900ce648854c3ab268ccc0fea8851cee143
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-28 08:38:07 -0500

    [initial] project Spring

---
+++ JavaExperto/project-desktop-SPRING/src/main/java/cpm/nestor/App.java
@@ -0,0 +1,13 @@
+package cpm.nestor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
