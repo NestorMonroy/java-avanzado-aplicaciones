c423f5f38c0f70283c4cea71e15fc32cb933a6e6
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-28 17:46:28 -0500

    [add] example ListEmployees

--- JavaExperto/project-WebServices/src/main/java/com.nestor.projectwebservices.ws/EmpleadosWS.java
+++ JavaExperto/project-WebServices/src/main/java/com.nestor.projectwebservices.ws/EmpleadosWS.java
@@ -8,16 +8,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("empleadosWS") //Nombre al cual acceder al web service
 public class EmpleadosWS {
 
-    private EmpleadoService empleadoService = new EmpleadoService();
+    private final EmpleadoService empleadoService = new EmpleadoService();
 
 
     @GET
     @Path("test")
-    public String test(){
+    public String test() {
 
         return "Example webservice con Jersey";
     }
@@ -26,7 +27,17 @@ public class EmpleadosWS {
     @Path("consultarEmpleado")
     @Produces(MediaType.APPLICATION_JSON)
     //@Consumes(MediaType.APPLICATION_JSON)
-    public Empleado consultarEmpleado(){
+    public Empleado consultarEmpleado() {
         return this.empleadoService.consultarEmpleado();
     }
+
+    @GET
+    @Path("consultarListaEmpleados")
+    @Produces(MediaType.APPLICATION_JSON)
+    //@Consumes(MediaType.APPLICATION_JSON)
+    public List<Empleado> consultarListaEmpleados() {
+        return this.empleadoService.consultarListaEmpleados();
+    }
+
+
 }
--- JavaExperto/project-WebServices/src/main/java/com/nestor/projectwebservices/service/EmpleadoService.java
+++ JavaExperto/project-WebServices/src/main/java/com/nestor/projectwebservices/service/EmpleadoService.java
@@ -3,6 +3,8 @@ package com.nestor.projectwebservices.service;
 import com.nestor.projectwebservices.entity.Empleado;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpleadoService {
     public Empleado consultarEmpleadoById(Long id) {
@@ -26,4 +28,27 @@ public class EmpleadoService {
 
     }
 
+    public List<Empleado> consultarListaEmpleados() {
+        List<Empleado> empleados = new ArrayList<>();
+        Empleado empleado = new Empleado();
+        empleado.setNumeroEmpleado("123456");
+        empleado.setNombre("Nestor");
+        empleado.setPrimerApellido("Monroy");
+        empleado.setSegundoApellido("Valero");
+        empleado.setFechaCreacion(LocalDateTime.now());
+        empleado.setEdad(20);
+
+        Empleado empleadoNuevo = new Empleado();
+        empleadoNuevo.setNumeroEmpleado("1234560");
+        empleadoNuevo.setNombre("Otro");
+        empleadoNuevo.setPrimerApellido("Otro");
+        empleadoNuevo.setSegundoApellido("Otro");
+        empleadoNuevo.setFechaCreacion(LocalDateTime.now());
+        empleadoNuevo.setEdad(20);
+
+        empleados.add(empleado);
+        empleados.add(empleadoNuevo);
+        return empleados;
+
+    }
 }
