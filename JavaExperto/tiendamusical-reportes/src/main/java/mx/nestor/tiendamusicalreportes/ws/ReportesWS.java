2f1a12688b0e00d4410ccbd653892e213a550b74
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-10-07 12:05:28 -0500

    [update] dependencies & [initial] WebService

---
+++ JavaExperto/tiendamusical-reportes/src/main/java/mx/nestor/tiendamusicalreportes/ws/ReportesWS.java
@@ -0,0 +1,8 @@
+package mx.nestor.tiendamusicalreportes.ws;
+
+/**
+ * @author NestorMonroy
+ * WebService que contendra los metodos configurados como servicios del WS.
+ */
+public class ReportesWS {
+}
