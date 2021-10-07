8c9d4693894ea2af261560c358fe53856567854c
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-10-07 13:17:05 -0500

    [add] test and modify dependencies jersey

--- JavaExperto/tiendamusical-reportes/src/main/java/mx/nestor/tiendamusicalreportes/ws/ReportesWS.java
+++ JavaExperto/tiendamusical-reportes/src/main/java/mx/nestor/tiendamusicalreportes/ws/ReportesWS.java
@@ -1,8 +1,21 @@
 package mx.nestor.tiendamusicalreportes.ws;
 
+
+import javax.ws.rs.*;
+import org.springframework.stereotype.Component;
+
 /**
  * @author NestorMonroy
  * WebService que contendra los metodos configurados como servicios del WS.
  */
+
+@Component
+@Path("/reportesWS")
 public class ReportesWS {
+
+    @GET
+    @Path("/pruebaWS")
+    public String pruebaWs(){
+        return "Ingresando al webservices";
+    }
 }
